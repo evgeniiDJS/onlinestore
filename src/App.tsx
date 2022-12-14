9935eb63768f1d4f97e0972ff7641916e37b4580
evgeniiDJS <hronologia2019@gmail.com>
2022-12-14 18:43:05 +0300

    init: startings project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Products } from "./Components/Products";
+
+function App() {
+
+
+
+ return (
+  <div className="container mx-auto max-w-2xl pt-5">
+    <Products />
+  </div>
+ )
+}
+
+export default App;
---
+++ src/Components/Products.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Products = () => {
+
+
+
+
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
