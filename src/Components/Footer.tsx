2703c9d78478e7a40611edf84bd02c5b142e4a50
evgeniiDJS <hronologia2019@gmail.com>
2023-01-11 11:53:41 +0300

    feat: add component footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,12 +1,7 @@
-
-
 export const Footer = () => {
-    
-
-    
-
-  
     return (
-     <div>Footre</div>
+     <div className="px-4 m-12 text-teal-500 italic">
+        <p>© 2022—2023 «MR. BERNER" Reprint and any reproduction of the materials and illustrations of the site or fragments thereof in any language is possible only with the written permission of MR. BERNER.</p>
+     </div>
     )
  }  
